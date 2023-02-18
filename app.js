11ababdef0b6b2f615648320f29449af1e727502
Nagum0 <121572632+Nagum0@users.noreply.github.com>
2023-02-18 16:22:28 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("Hello user who just inspected the page!")
+
+//Feladat #1
+const felOneLink = document.getElementById("felOneLink");
+let n = 45987612;
+let returnNum = "";
+
+felOneLink.addEventListener("click", ()=>{
+    returnNum = n.toString(2)
+    console.log(returnNum)
+});
